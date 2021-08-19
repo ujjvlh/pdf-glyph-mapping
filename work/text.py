6aa8e76cab117b1c70625de35cc686e8a322332a
ujjvlh <ujjwal.raajput@gmail.com>
2021-08-19 12:45:04 +0530

    र्ऋ->रृ

--- work/text.py
+++ work/text.py
@@ -10,11 +10,12 @@ for pg in pdftotext.PDF(open('../../gp-mbh/unabridged.fixed.pdf', 'rb')):
   c = pg
   while a != c:
     a = c
-    b = re.sub(r'(.)<CCsucc>(([क-हक़-य़]़?्)*[क-हक़-य़]़?)', r'\2\1', a)
-    c = re.sub(r'(([क-हक़-य़]़?्)*[क-हक़-य़ऋ][^क-हक़-य़ऋ]*)र्<CCprec>', r'र्\1', b)
+    c = re.sub(r'(.)<CCsucc>(([क-हक़-य़]़?्)*[क-हक़-य़]़?)', r'\2\1', a)
+    c = re.sub(r'(([क-हक़-य़]़?्)*[क-हक़-य़ऋ][^क-हक़-य़ऋ]*)र्<CCprec>', r'र्\1', c)
+    c = c.replace('र्ऋ', 'रृ')
   i += 1
-  with open('../../ujjvlh/gp-mbh/mbh/' + str(i) + '.html', 'w+') as f:
-    f.write(a.replace('\n', '\n<br>\n').replace(' ', '&nbsp;'))
+  with open('../../ujjvlh/gp-mbh/txt/' + str(i).rjust(5, '0') + '.txt', 'w+') as f:
+    f.write(a)
   s.append(f'<PAGE {i} BEGIN>\n' + a + f'\n<PAGE {i} END>')
   print(i)
 
